1b4b8aab80684e900103092954d566efc431f043
Wastard <Ilya.pakylov@gmail.com>
2023-03-22 20:10:26 +0300

    complete side panel design

--- src/components/Buttons/Glyf/Glyf.vue
+++ src/components/Buttons/Glyf/Glyf.vue
@@ -10,20 +10,11 @@
 <template>
     <div @mouseleave="isHint = !isHint" @mouseenter="isHint=!isHint" type="button" :id="css.glyf_container" :key="key">   
         <img :id="css.glyf_img" v-if="Key === 'Каталог'" src="https://img.icons8.com/fluency-systems-regular/200/FFE500/shopping-bag--v1.png" :alt=Key>
-        <div :id="css.glyf_hint" v-if="Key === 'Каталог' && isHint">
-            <h6>{{ Key }}</h6>
-        </div>
         <img :id="css.glyf_img" v-else-if="Key === 'Корзина'" src="https://img.icons8.com/fluency-systems-regular/200/FFE500/shopping-basket.png" :alt=Key>
-        <div :id="css.glyf_hint" v-if="Key === 'Корзина' && isHint">
-            <h6>{{ Key }}</h6>
-        </div>
-        <img :id="css.glyf_img" v-else-if="Key === 'Избранное'" src="https://img.icons8.com/windows/200/FFE500/hearts.png" :alt=Key>
-        <div :id="css.glyf_hint" v-if="Key === 'Избранное' && isHint">
-            <h6>{{ Key }}</h6>
-        </div>
         <img :id="css.glyf_img" v-else-if="Key === 'Контакты'" src="https://img.icons8.com/fluency-systems-regular/200/FFE500/telegram-app.png" :alt=Key>
-        <div :id="css.glyf_hint" v-if="Key === 'Контакты' && isHint">
-            <h6>{{ Key }}</h6>
+        <img :id="css.glyf_img" v-else-if="Key === 'Избранное'" src="https://img.icons8.com/windows/200/FFE500/hearts.png" :alt=Key>
+        <div :class="{[css.glyf_hint]:true, [css.glyf_hint_show]: isHint}">
+            <h6 :id="css.glyf_text" >{{ Key }}</h6>
         </div>
     </div>
 </template>
