b86834df1139470b393bf2a13bf55fc4b3dbf109
Wastard <Ilya.pakylov@gmail.com>
2023-03-31 01:42:21 +0300

    add sign window design

--- src/components/SignPanel/SignPanel.vue
+++ src/components/SignPanel/SignPanel.vue
@@ -12,6 +12,21 @@
             <div :id="css.window_header" >
                 <img @click="$emit('changeWindow')" type="button" :id="css.sign_close_img" src="https://img.icons8.com/material/200/FFE500/multiply--v1.png" alt="">
             </div>
+            <div :id="css.window_title">Вход</div>
+            <div :id="css.window_form">
+                <div :id="login_container">
+                    <p :id="css.form_text">Логин</p>
+                    <input :id="css.sign_input" type="email">
+                </div>
+                <div :id="password_container">
+                    <p :id="css.form_text">Пароль</p>
+                    <input :id="css.sign_input" type="password" >
+                </div>
+            </div>
+            <div :id="css.button_container">
+                <a :id="css.form_button" type="button">Войти</a>
+            </div>
+            <div :id="css.change_form" type="button">Впервые у нас? Зарегистрируйтесь!</div>
         </div>
     </div>
 </template>
